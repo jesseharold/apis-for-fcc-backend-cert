98e6c178df1fb4ba711a2fe2c77166dad489f5d7
Harold <jesseharold@gmail.com>
2017-05-05 22:34:57 +0000

    testing string complete

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+function isTimestamp(string){
+    // is this string all digits?
+    var re = /^\d+$/;
+    return re.test(string);
+}
+
+function isNatural(string){
+    string.replace(/%20/, " ");
+    var re = /(january|february|march|april|may|june|july|august|september|october|november|december)\s\d+,\s\d\d\d\d/i;
+    return re.test(string);
+}
+
+function convertToNatural(string){
+    return string+"-natural";
+}
+
+function convertToTimestamp(string){
+    return string+"-timestamp";
+}
+
+app.get('/:timestamp', function (req, res) {
+    var responseObject = {"unix": null, "natural": null};
+    var input = req.params["timestamp"];
+    if (isTimestamp(input)){
+        responseObject.unix = input;
+        responseObject.natural = convertToNatural(input);
+    } else if (isNatural(input)){
+        responseObject.natural = input;
+        responseObject.unix = convertToTimestamp(input);
+    }
+    res.send(responseObject);
 })
 
 app.listen(8080, function () {
